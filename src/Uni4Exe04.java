c849ac4c980ef60db3fe1e63a317255e08795da7
Ari Elias da Silva Júnior <ari@furb.br>
2022-04-04 19:11:50 -0300

    Informa se o número tem casas decimais

--- src/Uni4Exe04.java
+++ src/Uni4Exe04.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class Uni4Exe04 {
-    
+     public static void main(String[] args) {
+         // Scanner
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Informe um número: ");
+        double numero = teclado.nextDouble();
+
+        teclado.close();
+        //Scanner
+
+        if(numero % 1 ==0){
+            System.out.println("Ele não tem casas decimais");
+        } else {
+            System.out.println("Ele tem casas decimais");
+        }
+
+        System.out.println("Fim");
+     }
 }
