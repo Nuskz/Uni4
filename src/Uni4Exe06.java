3030bcc1a4aef201aaa9f4c56933d7a3c989dbb4
Ari Elias da Silva Júnior <ari@furb.br>
2022-04-04 19:58:39 -0300

    Informar genero

---
+++ src/Uni4Exe06.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Uni4Exe06 {
+    public static void main(String[] args) {
+        // Scanner
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Informe o seu genêro:\n (M)asculino\n (F)eminino\n Não (I)nformar");
+        String genero = teclado.nextLine();
+        teclado.close();
+        // Scanner fechado
+
+        genero = genero.trim().toUpperCase();
+
+        if( genero.equals("M")){
+            System.out.println("Masculino");
+        } else if( genero.equals("F")){
+            System.out.println("Feminino");
+        } else if(genero.equals("I") ){
+            System.out.println("Não informado");
+        } else{
+            System.out.println("Entrada Incorreta");
+        }
+
+
+
+    }
+}
