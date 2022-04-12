8f5b5d9e54489741d5c890e8e3813ac4ccd655fb
Nuskz <arielliasjr200@gmail.com>
2022-04-12 16:00:40 -0300

    cursos (incompleto)

---
+++ src/Uni4Exe22.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Uni4Exe22 {
+    public static void main(String[] args) {
+        // Scanner
+        Scanner teclado = new Scanner(System.in);
+        // Opções
+        System.out.println(
+                "Qual desses cursos é o seu?\n 1-Ciência da Computação\n 2-Licenciatura da Computação\n 3-Sistemas de Informação");
+        int opcao = teclado.nextInt();
+
+        teclado.close();
+        // Fim do Scanner
+        switch (opcao) {
+            // Ciencia da computacao
+            case 1:
+                System.out.println("Bacharel em Ciência da Computação");
+                break;
+
+            // Licenciatura da computacao
+            case 2:
+                System.out.println("Licenciado em Computação");
+        }
+
+    }
+
+}
